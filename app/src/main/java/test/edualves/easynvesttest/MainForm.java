545553bf957d092dfec9a97c56688bd5684d3c8d
ealves <ealves@moobie.com.br>
2017-05-25 16:08:37 -0300

    Init commit

---
+++ app/src/main/java/test/edualves/easynvesttest/MainForm.java
@@ -0,0 +1,13 @@
+package test.edualves.easynvesttest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_form);
+    }
+}
